961ba12e015713b115c2cd2ddda713a3f661b015
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2023-02-07 22:23:39 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+template <class T>
+class vector
+{
+    T *arr;
+    int size;
+};
+ 
+int main()
+{
+    vector<int> v1();
+    vector<float> v2();
+}
